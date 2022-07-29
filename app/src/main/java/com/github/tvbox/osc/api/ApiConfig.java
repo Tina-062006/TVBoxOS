8e1640ff6349a8400810765190532b929089cec9
tina062006 <90503856+tina062006@users.noreply.github.com>
2022-07-29 21:09:37 +0800

    内置的666888的地址

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -80,7 +80,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://www.kf666888.cn/api/tvbox?peizhi=666&to=qj&to=ts");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
